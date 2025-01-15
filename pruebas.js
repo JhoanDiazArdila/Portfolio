7ba4da672901afe88d04f2a575695096c678512a
JhoanDiazArdila <jhoan.diaz.ardila@hotmail.com>
2025-01-15 15:52:12 -0500

    feat: :construction: Web page in progress
    Js code modify, change of new files as Primary -runner.js-
    
    by: JhoanDiaz

--- pruebas.js
+++ pruebas.js
@@ -12,79 +12,258 @@ function activelink() {
 list.forEach((item) => item.addEventListener("mouseover", activelink));
 let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
+// let main = document.querySelector(".main");
 
 
-let botonIniciar = document.querySelector(".iniciar")
-let menu = document.querySelector(".header-container");
-let welcome = document.querySelector(".welcome");
-let first = document.querySelector(".first");
-let introAbout = document.querySelector(".introAbout");
-let soyFoto = document.querySelector(".foto");
-let soyTexto = document.querySelector(".texto");
-console.log(toggle, navigation, main);
+// let botonIniciar = document.querySelector(".iniciar")
+// let menu = document.querySelector(".header-container");
+// let welcome = document.querySelector(".welcome");
+// let first = document.querySelector(".first");
+// let introAbout = document.querySelector(".introAbout");
+const soyFoto = document.querySelector(".foto");
+// let soyTexto = document.querySelector(".texto");
+// console.log(toggle, navigation, main);
 
 
 // HACER CLICK TOGGLE
 toggle.onclick = function () {
-    navigation.classList.toggle("active");
+  navigation.classList.toggle("active");
+  soyFoto.classList.toggle("active");
+    
 
-    soyFoto.classList.toggle("active");
+    
     // main.classList.toggle("active");
     // menu.classList.toggle("active");
-
 };
 
-// HACER CLICK EN GO!
-botonIniciar.onclick = function(){
-    menu.classList.toggle("active");
-    first.classList.toggle("active"); //Desaparece Introduccion
-    botonIniciar.classList.toggle("active"); //Desaparece boton
 
-    // Aparece Think luego se arregla el main
-    introAbout.style.display = 'flex'; // Se asegura de estar en el flujo
-    setTimeout(() => {
-        introAbout.classList.add('active'); // Aparece suavemente
 
-        // Desaparece suavemente después de 1.2s
-        setTimeout(() => {
-            introAbout.classList.remove('active'); // Comienza a desaparecer
-            introAbout.classList.add('inactive'); // Controla opacidad a 0
+
+
+
+
+
+
+// CARGA DE DATOS DESDE JSON
+// funcion para cargar el json segun lo seleccionado
+document.addEventListener('DOMContentLoaded', () => {
+  const content = document.getElementById('content');
+  const languageSelector = document.getElementById('languageSelector');
+  let currentLanguage = 'en';// esto determina que por predeterminado sea en ingles a menos que se cambie
+
+  async function loadContent(section) {
+    try {
+      const response = await fetch(`content/content_${currentLanguage}.json`);
+      if (!response.ok) throw new Error('Error al cargar el archivo JSON');
+      const data = await response.json();//carga del json
+
+
+      switch (section) {
+
+        case 'intro':
+          let introHtml = `
+            <h5 class="welcome">${data.intro.welcome}</h5>
+
+            <section class="first">
+              <h3 class="greeting">${data.first.greeting}</h3>
+              <div class="whoAmI">
+                <p class="this">${data.first.whoAmI}</p>
+                <h1>${data.first.name}</h1>
+              </div>
+              <h4 class="title">${data.first.title1}</h4>
+
+              <p class="text">${data.first.text}</p>
+              <p class="text1">${data.first.text1}</p>
+              
+              ${data.first.sub_text.map(line => `<p class="sub-text">${line}</p>`).join('')}
+
+              <div class="divisionFlechasGo">
+                <ion-icon class="flechaGo" name="arrow-redo-sharp"></ion-icon>
+                <button class="iniciar">${data.first.button_Go}</button>
+                <ion-icon class="flechaGo" name="arrow-undo-sharp"></ion-icon>
+              </div>
+            </section>
+            <h5 class="introAbout">${data.intro_about.think}</h5>
+  
+            <section class="foto">
+              <img src="${data.aboutMe.imgAboutMe}" alt="foto de perfil">
+              <h2>${data.aboutMe.name}</h2>
+              <h6>${data.aboutMe.occupation}</h6>
+              <div class="logosImagen">
+                <a href="${data.aboutMe.imgGit}" target="_blank" rel="noopener noreferrer">
+                    <ion-icon name="logo-github"></ion-icon>
+                </a>
+                <a href="${data.aboutMe.imgLinkedin}" target="_blank" rel="noopener noreferrer">
+                    <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a href="${data.aboutMe.imgWhatsApp}" target="_blank" rel="noopener noreferrer">
+                    <ion-icon name="logo-whatsapp"></ion-icon>
+                </a>
+              </div>
+            </section>
+            
+            <section class="texto">
+              <h4>${data.aboutMe.title}</h4>
+              <div>
+                ${data.aboutMe.parrafo1.map(line => `<p>${line}</p>`).join('')}
+              </div>
+              <div>
+                ${data.aboutMe.parrafo2.map(line => `<p>${line}</p>`).join('')}
+              </div>
+              <div>
+                ${data.aboutMe.parrafo3.map(line => `<p>${line}</p>`).join('')}
+              </div>
+              <div id="flechasScroll">
+                <a href="">
+                    <ion-icon name="arrow-down-circle"></ion-icon>
+                </a>
+                <a href="">
+                    <ion-icon name="arrow-up-circle"></ion-icon>
+                </a>
+              </div>
+            </section>
+          `;
+
+          content.innerHTML = introHtml;
+
+          const welcome = document.querySelector(".welcome");
+          const first = document.querySelector(".first");
+          const botonIniciar = document.querySelector(".iniciar");
+          const main = document.querySelector(".main");
+
+          if (welcome) {
+            setTimeout(() => {
+              welcome.classList.add('active'); // Agrega la clase para ocultarlo
+              setTimeout(() => {
+                welcome.style.display = 'none'; // Quita completamente el elemento
+              }, 1500);
+            }, 1500); // 1500 ms = 1.5 segundos
 
             setTimeout(() => {
-                introAbout.style.display = 'none'; // Remueve completamente del flujo
-                introAbout.classList.remove('inactive'); // Limpia la clase
-                main.classList.toggle("active"); // Muestra el contenido principal
-                soyFoto.style.display = 'flex';
-                soyTexto.style.display = 'flex';
-            }, 1000); // Tiempo de transición de opacidad (1s)
-        }, 1200); // Tiempo de visibilidad de "Think..." (1.2s)
-    }, 300); // Retraso antes de mostrar "Think..." (300ms)
-};
+              botonIniciar.classList.add('active');
+              first.classList.add('active');
+            }, 3100);
+          }
+          
+          if (first) {
+            setTimeout(() => {
+              first.classList.add('active'); // Muestra el contenido de "first"
+            }, 3100);
+          }
 
-// DESVANECER el WELCOME
-setTimeout(() => {
-    welcome.classList.add('active'); // Agrega la clase para ocultarlo
+          if (botonIniciar) {
+            console.log(' 1 llego aqui!!!!!!!!!');
+            
+            botonIniciar.addEventListener('click', () => {
+              console.log('2 llego aqui!!!!!!!!!');
+              const menu = document.querySelector(".header-container");
+              const introAbout = document.querySelector(".introAbout"); // No ha cargado
+              const soyFoto = document.querySelector(".foto"); // No ha cargado
+              const soyTexto = document.querySelector(".texto"); // No ha cargado
+              console.log('3 llego aqui!!!!!!!!!');
 
-    setTimeout(() => {
-        welcome.style.display = 'none'; // Quita completamente el elemento
-    }, 1500);  
-}, 1500); // 1500 ms = 1.5 segundos
+              if (menu && first) {
+                console.log('4 llego aqui!!!!!!!!!');
+                menu.classList.add("active");
+                first.classList.toggle("active");
+                botonIniciar.classList.toggle("active");
 
-// APARECER BOTON GO y Demas
-setTimeout(() => {
-    botonIniciar.classList.add('active'); 
-    first.classList.add('active');
-}, 3100);
+                console.log('5 llego aqui!!!!!!!!!');
+                
+                introAbout.style.display = 'flex';
+                setTimeout(() => {
+                  introAbout.classList.add('active');
+                  setTimeout(() => {
+                    introAbout.classList.remove('active');
+                    introAbout.classList.add('inactive');
+                    setTimeout(() => {
+                      introAbout.style.display = 'none';
+                      introAbout.classList.remove('inactive');
+                      main.classList.toggle("active");
+                      soyFoto.style.display = 'flex';
+                      soyTexto.style.display = 'flex';
+                    }, 1000);
+                  }, 1200);
+                }, 300);
+              }
+            });
+          }
+          break;
 
 
+        case 'aboutMe':
+          let aboutMe = `
+            <h5 class="introAbout">${data.intro_about.think}</h5>
+  
+            <section class="foto">
+              <img src="${data.aboutMe.imgAboutMe}" alt="foto de perfil">
+              <h2>${data.aboutMe.name}</h2>
+              <h6>${data.aboutMe.occupation}</h6>
+              <div class="logosImagen">
+                <a href="${data.aboutMe.imgGit}" target="_blank" rel="noopener noreferrer">
+                    <ion-icon name="logo-github"></ion-icon>
+                </a>
+                <a href="${data.aboutMe.imgLinkedin}" target="_blank" rel="noopener noreferrer">
+                    <ion-icon name="logo-linkedin"></ion-icon>
+                </a>
+                <a href="${data.aboutMe.imgWhatsApp}" target="_blank" rel="noopener noreferrer">
+                    <ion-icon name="logo-whatsapp"></ion-icon>
+                </a>
+              </div>
+            </section>
+            
+            <section class="texto">
+              <h4>${data.aboutMe.title}</h4>
+              <div>
+                ${data.aboutMe.parrafo1.map(line => `<p>${line}</p>`).join('')}
+              </div>
+              <div>
+                ${data.aboutMe.parrafo2.map(line => `<p>${line}</p>`).join('')}
+              </div>
+              <div>
+                ${data.aboutMe.parrafo3.map(line => `<p>${line}</p>`).join('')}
+              </div>
+              <div id="flechasScroll">
+                <a href="">
+                    <ion-icon name="arrow-down-circle"></ion-icon>
+                </a>
+                <a href="">
+                    <ion-icon name="arrow-up-circle"></ion-icon>
+                </a>
+              </div>
+            </section>
+          `;
+          content.innerHTML = aboutMe;
+          break;
 
 
+          
+        default:
+          content.innerHTML = `<p>Section not found.</p>`;
+          break;
+      }
+    } catch (error) {
+        console.error('Error:', error);
+        content.innerHTML = `<p>Error loading content.</p>`;
+    }
+  }
+  if (languageSelector) {
+    languageSelector.addEventListener('click', () => {
+      // Alternar entre 'en' y 'es' (o el idioma que desees)
+      currentLanguage = currentLanguage === 'en' ? 'es' : 'en';
+      loadContent('intro');
+    });
+  }
+
+  const aboutMeButton = document.getElementById('AboutMe');
+    if (aboutMeButton) {
+      // document.getElementById('AboutMe').addEventListener('click', () => loadContent('aboutMe'));
+      aboutMeButton.addEventListener('click', () => loadContent('aboutMe'));
+    }
 
 
-
-
-
+  loadContent('intro');
+});
 
 
 
--- runner.js
+++ runner.js
@@ -1,231 +1,137 @@
-
-
-
 //Estilos barra de inicio
 let list = document.querySelectorAll(".navigation li");
 
 function activelink() {
-  list.forEach((element) => {
-    element.classList.remove("hovered");
-  });
-  this.classList.add("hovered");
+    list.forEach((element) => {
+        element.classList.remove("hovered");
+    });
+    this.classList.add("hovered");
 }
-list.forEach((item) => item.addEventListener("mouseover", activelink));
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
-console.log(toggle, navigation, main);
 
+// INDICADORES DE CLASES
+list.forEach((item) => item.addEventListener("mouseover", activelink));
+const toggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+const main = document.querySelector(".main");
+
+
+const botonIniciar = document.querySelector(".iniciar")
+const menu = document.querySelector(".header-container");
+const welcome = document.querySelector(".welcome");
+const first = document.querySelector(".first");
+const introAbout = document.querySelector(".introAbout");
+const soyFoto = document.querySelector(".foto");
+const soyTexto = document.querySelector(".texto");
+
+const flechaBajarAbout = document.querySelector("#bajarAbout");
+const flechaSubirAbout = document.querySelector("#subirAbout");
+
+const principles = document.querySelector(".principles");
+const flechaBajarPrinci = document.querySelector("#bajarPrinci");
+const flechaSubirPrinci = document.querySelector("#subirPrinci");
+
+const mision = document.querySelector(".mision");
+
+
+
+
+// HACER CLICK TOGGLE
 toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
-  
+    navigation.classList.toggle("active");
+
+    soyFoto.classList.toggle("active");
+    // main.classList.toggle("active");
+    // menu.classList.toggle("active");
+
+};
+
+// DESVANECER el WELCOME
+setTimeout(() => {
+  welcome.classList.add('active'); // Agrega la clase para ocultarlo
+
+  setTimeout(() => {
+      welcome.style.display = 'none'; // Quita completamente el elemento
+  }, 1500);  
+}, 1500); // 1500 ms = 1.5 segundos
+
+// APARECER BOTON GO y Demas
+setTimeout(() => {
+  botonIniciar.classList.add('active'); 
+  first.classList.add('active');
+}, 3100);
+
+// HACER CLICK EN GO!
+botonIniciar.onclick = function(){
+    menu.classList.toggle("active");
+    first.classList.toggle("active"); //Desaparece Introduccion
+    botonIniciar.classList.toggle("active"); //Desaparece boton
+
+    // Aparece Think luego se arregla el main
+    introAbout.style.display = 'flex'; // Se asegura de estar en el flujo
+    setTimeout(() => {
+        introAbout.classList.add('active'); // Aparece suavemente
+
+        // Desaparece suavemente después de 1.2s
+        setTimeout(() => {
+            introAbout.classList.remove('active'); // Comienza a desaparecer
+            introAbout.classList.add('inactive'); // Controla opacidad a 0
+
+            setTimeout(() => {
+                introAbout.style.display = 'none'; // Remueve completamente del flujo
+                introAbout.classList.remove('inactive'); // Limpia la clase
+                main.classList.toggle("active"); // Muestra el contenido principal
+                soyFoto.style.display = 'flex';
+                soyTexto.style.display = 'flex';
+            }, 1000); // Tiempo de transición de opacidad (1s)
+        }, 1200); // Tiempo de visibilidad de "Think..." (1.2s)
+    }, 300); // Retraso antes de mostrar "Think..." (300ms)
 };
 
 
-// funcion para cargar el json segun lo seleccionado
-document.addEventListener('DOMContentLoaded', () => {
-  const content = document.getElementById('content');
-  const languageSelector = document.getElementById('languageSelector');
-  let currentLanguage = 'en';// esto determina que por predeterminado sea en ingles a menos que se cambie
+flechaBajarAbout.onclick = function(){
+  soyFoto.style.display = 'none';
+  soyTexto.style.display = 'none'
+  main.classList.remove("active");
+  main.classList.add("nodivision");
+  principles.classList.add("active");
 
-  async function loadContent(section) {
-    try {
-      const response = await fetch(`content/content_${currentLanguage}.json`);
-      if (!response.ok) throw new Error('Error al cargar el archivo JSON');
-      const data = await response.json();//carga del json
+};
 
-
-      switch (section) {
-
-        case 'intro':
-          let introHtml =`
-            <h5 class="welcome">${data.intro.welcome}</h5>
-            <section class="first">
-              <h3 class="greeting">${data.first.greeting}</h3>
-              <div class="whoAmI">
-                <p class="this">${data.first.whoAmI}</p>
-                <h1>${data.first.name}</h1>
-              </div>
-              <h4 class="title">${data.first.title1}</h4>
-
-              <p class="text">${data.first.text}</p>
-              <p class="text1">${data.first.text1}</p>
-
-              <p class="sub-text">${data.first.sub_text}</p>
-              ${data.first.sub_text.map(line => <p class="sub-text">${line}</p>).join('')}
-
-              <div class="divisionFlechasGo">
-                <ion-icon class="flechaGo" name="arrow-redo-sharp"></ion-icon>
-                <button class="iniciar">${data.first.button_Go}</button>
-                <ion-icon class="flechaGo" name="arrow-undo-sharp"></ion-icon>
-              </div>
-            </section>
-          `;
-          content.innerHTML = introHtml;
-          break;
-
-        case 'aboutMe':
-          let aboutMe = `
-            <h5 class="introAbout">${data.intro_about.think}</h5>
-
-            <section class="foto">
-              <img src="${data.aboutMe.imgAboutMe}" alt="foto de perfil">
-              <h2>${data.aboutMe.name}</h2>
-              <h6>${data.aboutMe.occupation}</h6>
-              <div class="logosImagen">
-                <a href="${data.aboutMe.imgGit}" target="_blank" rel="noopener noreferrer">
-                    <ion-icon name="logo-github"></ion-icon>
-                </a>
-                <a href="${data.aboutMe.imgLinkedin}" target="_blank" rel="noopener noreferrer">
-                    <ion-icon name="logo-linkedin"></ion-icon>
-                </a>
-                <a href="${data.aboutMe.imgWhatsApp}" target="_blank" rel="noopener noreferrer">
-                    <ion-icon name="logo-whatsapp"></ion-icon>
-                </a>
-              </div>
-            </section>
-            
-
-            <section class="texto">
-              <h2>Hola soy texto</h2>
-            </section>
-
-          `;
-          content.innerHTML = aboutMe;
-          break;// intro va hasta aqui
-
-
-// COMPLETADO HASTA AQUI FALTA ARREGLAR ABOUTME
+flechaBajarPrinci.onclick = function(){
+  principles.classList.remove("active");
+  main.classList.remove("nodivision");
+  main.classList.add("active");
+  soyFoto.style.display = 'flex';
+  mision.style.display = 'flex';
+};
 
 
 
-        case 'aboutMe':
-          content.innerHTML = `
-                      <section id="aboutMe">
-                          <article>
-                              <h2>${data.aboutMe.name}</h2>
-                              <h3>${data.aboutMe.occupation}</h3>
-                              <img src=${data.aboutMe.imgAboutMe} alt="imgAboutMe">
-                          </article>
-                          <article>
-                              <h3>${data.aboutMe.title}</h3>
-                              <p>${data.aboutMe.occupation}</p>
-                            
-                              <p>${data.aboutMe.vision}</p>
-                              <p>${data.aboutMe.mission}</p>
-                              <h3>Principles</h3>
-                              <p>${data.aboutMe.principles}</p>
-                          </article>
-                        
-                      </section>
-                  `;
-          break;
+/*
+// FUNCION DE SCROLL
+
+let lastScrollTop = 0;  // Inicializamos la última posición del scroll
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;  // Obtenemos la posición actual del scroll
+
+    if (currentScroll > lastScrollTop) {
+      console.log("Scroll hacia abajo");
+      soyFoto.style.display = 'flex';
+      soyTexto.style.display = 'flex'
+      main.classList.toggle("active");
+
+      principles.classList.add("active");
+
+    } else {
+      console.log("Scroll hacia arriba");
 
 
-        case 'services':
-          content.innerHTML = `
-                      <section id="services">
-                          <h2>My Services</h2>
-                          <p>${data.services.description}</p>
-                          <h3>Featured Projects</h3>
-                          <ul>
-                              ${data.services.projects.map(project => `<li>${project}</li>`).join('')}
-                          </ul>
-                          <h3>Programming Languages</h3>
-                          <p>${data.services.languages.join(', ')}</p>
-                      </section>
-                  `;
-          break;
-
-
-        case 'contact':
-          content.innerHTML = `
-                      <section id="contact">
-                          <h2>Contact Me</h2>
-                          <form>
-                              <label for="name">${data.contact.form.name}:</label>
-                              <input type="text" id="name" name="name" required>
-
-
-                              <label for="email">${data.contact.form.email}:</label>
-                              <input type="email" id="email" name="email" required>
-
-
-                              <label for="message">${data.contact.form.message}:</label>
-                              <textarea id="message" name="message" required></textarea>
-
-
-                              <button type="submit">${data.contact.submitText}</button>
-                          </form>
-                      </section>
-                  `;
-          break;
-
-
-        default:
-          content.innerHTML = `<p>Section not found.</p>`;
-          break;
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      content.innerHTML = `<p>Error loading content.</p>`;
     }
-  }
 
-
-  languageSelector.addEventListener('change', (event) => {
-    currentLanguage = event.target.value;
-    loadContent('intro');
-  });
-
-
-  document.getElementById('#AboutMe').addEventListener('click', () => loadContent('aboutMe'));
-
-
-
-  loadContent('intro');
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;  // Actualizamos la última posición del scroll
 });
+*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// botonesHeader.forEach(boton=> boton.addEventListener("click",(event)=>{
-//     const botonId = event.currentTarget.id;
-//     formularioEntrada.classList.remove("contenedor-ocupacion");
-//     formularioEntrada.classList.add("contenedor-formulario");
-//     formularioEntrada.innerHTML="";
-//     // descripcion.innerHTML="";
-
-//     switch(botonId){
-//         case "registro_entrada":
-//             mostrar_Formulario_Entrada();
-//             break;
-//         case "registro_salida":
-//             mostrar_Formulario_Salida();
-//             break;
-//         case "registro_modificar":
-//             tablaModificarDatos();
-//             break;
-//         case "registro_ocupacion":
-//             ocupacion ();
-//             break;
-//         case "ver_registro":
-//             mostrarTablaHistorial ();
-//             break;
-//         case "otra-imagen":
-//             mostrarInicio();
-//             break;
-//     }
-// }));
